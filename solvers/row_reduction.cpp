b9bc006d6fb7ebd220ce63e82cfb856f2cfb0380
ExtraWack <166250485+ExtraWack@users.noreply.github.com>
2024-05-30 17:51:25 -0700

    actually made the row reduction function (may be buggy)
    made the row reduction solver. is probably buggy and idk if it works cause i actually havent compiled or tested it

--- solvers/row_reduction.cpp
+++ solvers/row_reduction.cpp
@@ -5,14 +5,42 @@ using namespace Eigen;
 using namespace std;
 
 vector<tuple<Matrix<float, Dynamic, Dynamic>, string>> RowReductionSolver::solve(Matrix<float, Dynamic, Dynamic> mat) {
+    //initial set up 
+    r = mat.rows();
     vector<tuple<Matrix<float, Dynamic, Dynamic>, string>> steps;
+    string cur_step = ""
 
-    Matrix<float, Dynamic, Dynamic> result = Matrix<float, Dynamic, Dynamic>::Zero(rowsA, colsB);
+    //for each row
+    for(int row = 0; row < r; row++)
+    {   //for each row below this row
+        string cur_row = to_string(row);
+        string cur_piv = to_string(mat(row,row));
 
+        for(int brow = row; brow < brow++ )
+        {
+            string bel_row = to_string(brow);
+            string bel_piv = to_string(mat(brow,row));
+            if(mat(row,row) != 0)
+            {
+                float factor = -1*(mat(brow,row)/mat(row,row));
+                //multiply it by factor, then add to brow
+                mat.row(brow) += factor * mat.row(row);
+                //multiply row "row" by "-(row,row)/(brow,row)" and add it to row "brow"
+                cur_step = "multply row " + cur_row + " by " + bel_piv + "/" + cur_piv + " and add to " + bel_row;
+                steps.push_back((mat,cur_step));
+            }
+            else
+            {
+                cur_step = "a pivot position is 0, thefore this matrix can not be reduced further."
+                steps.push_back((mat,cur_step));
+                return steps;
+            }
+        }
+    }
     return steps;
 }
 
 bool RowReductionSolver::verify(Matrix<float, Dynamic, Dynamic> a) {
-    // A matrix can always be Reduced
-    return true
+    // this program can only reduce square matrices
+    return (a.rows() == a.col());
 }
