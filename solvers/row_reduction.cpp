22e3a78d7e03e59e2f76856500177711d2eac7f5
ExtraWack <166250485+ExtraWack@users.noreply.github.com>
2024-05-30 18:47:25 -0700

    fixed compile errors of row_reduction solver
    does that mean it works? probably not :)

--- solvers/row_reduction.cpp
+++ solvers/row_reduction.cpp
@@ -1,4 +1,4 @@
-#include <solvers.hpp>
+#include "solvers.hpp"
 #include <stdexcept>
 
 using namespace Eigen;
@@ -6,17 +6,17 @@ using namespace std;
 
 vector<tuple<Matrix<float, Dynamic, Dynamic>, string>> RowReductionSolver::solve(Matrix<float, Dynamic, Dynamic> mat) {
     //initial set up 
-    r = mat.rows();
+    int r = mat.rows();
     vector<tuple<Matrix<float, Dynamic, Dynamic>, string>> steps;
-    string cur_step = ""
-
+    string cur_step = "";
+    tuple push = {mat, cur_step};
     //for each row
     for(int row = 0; row < r; row++)
     {   //for each row below this row
         string cur_row = to_string(row);
         string cur_piv = to_string(mat(row,row));
 
-        for(int brow = row; brow < brow++ )
+        for(int brow = row; brow < r; brow++ )
         {
             string bel_row = to_string(brow);
             string bel_piv = to_string(mat(brow,row));
@@ -27,12 +27,14 @@ vector<tuple<Matrix<float, Dynamic, Dynamic>, string>> RowReductionSolver::solve
                 mat.row(brow) += factor * mat.row(row);
                 //multiply row "row" by "-(row,row)/(brow,row)" and add it to row "brow"
                 cur_step = "multply row " + cur_row + " by " + bel_piv + "/" + cur_piv + " and add to " + bel_row;
-                steps.push_back((mat,cur_step));
+                push = {mat, cur_step};
+                steps.push_back(push);
             }
             else
             {
-                cur_step = "a pivot position is 0, thefore this matrix can not be reduced further."
-                steps.push_back((mat,cur_step));
+                cur_step = "a pivot position is 0, thefore this matrix can not be reduced further.";
+                push = {mat, cur_step};
+                steps.push_back(push);
                 return steps;
             }
         }
@@ -42,5 +44,5 @@ vector<tuple<Matrix<float, Dynamic, Dynamic>, string>> RowReductionSolver::solve
 
 bool RowReductionSolver::verify(Matrix<float, Dynamic, Dynamic> a) {
     // this program can only reduce square matrices
-    return (a.rows() == a.col());
+    return (a.rows() == a.cols());
 }
